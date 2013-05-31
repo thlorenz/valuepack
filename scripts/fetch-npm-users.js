72a5ef71a66018a14c418a0211433651243f2064
Thorsten Lorenz <thlorenz@gmx.de>
2013-05-31 07:51:15 -0400

    reorganizing fetch npm users script

--- scripts/fetch-npm-users.js
+++ scripts/fetch-npm-users.js
@@ -12,38 +12,43 @@ var path       =  require('path')
   , dblocation =  path.join(__dirname, '..', 'store/valuepack.db')
   ;
 
-function retrieveOnly() {
-  level(dblocation, { valueEncoding: 'json' }, function (err, db) {
-    if (err) return console.error(err);
-    db = sublevel(db);
+function retrieveOnly(db, cb) {
+  db = sublevel(db);
 
-    var users    =  db.sublevel(npm.users, { valueEncoding: 'json' })
-      , byLogin  =  db.sublevel(npm.byLogin, { valueEncoding: 'utf8' })
-      , byGithub =  db.sublevel(npm.byLogin, { valueEncoding: 'utf8' });
+  var users    =  db.sublevel(npm.users, { valueEncoding: 'json' })
+    , byLogin  =  db.sublevel(npm.byLogin, { valueEncoding: 'utf8' })
+    , byGithub =  db.sublevel(npm.byLogin, { valueEncoding: 'utf8' });
 
-    dump.keys(byGithub)
-  })
+  dump.keys(byGithub, function(err) { cb(err, db) })
 }
 
-function fetchAndStoreNpmUsers(cb) {
+function fetchAndStoreNpmUsers(db, cb) {
   // TODO: request this file from couch every time
   // https://registry.npmjs.org/-/users/
   var json = fs.readFileSync(path.join(__dirname, '..', 'data', 'npm-users.json'), 'utf8')
-
-  level.destroy(dblocation, function () {
-    level(dblocation, { valueEncoding: 'json' }, function (err, db) {
-      if (err) return console.error(err);
-      db = sublevel(db);
-      store(db, json,  function (err, subs) {
-        if (err) return console.error(err);
-        console.log('Stored all npm users at: ', dblocation);
-        cb()
-      })
-    })
+  db = sublevel(db);
+  store(db, json,  function (err, subs) {
+    if (err) return console.error(err);
+    console.log('Stored all npm users at: ', dblocation);
+    cb(null, db)
   })
 }
 
 if (~process.argv.indexOf('--fresh'))
-  fetchAndStoreNpmUsers(retrieveOnly)
+  level.destroy(dblocation, function () {
+    level(dblocation, { valueEncoding: 'json' }, function (err, db) {
+      if (err) return done(err, db);
+      fetchAndStoreNpmUsers(db, retrieveOnly.bind(null, db, done))
+    })
+  })
 else 
-  retrieveOnly()
+  level(dblocation, { valueEncoding: 'json' }, function (err, db) {
+    if (err) return done(err, db);
+    retrieveOnly(db, done)
+  })
+
+function done(err, db) {
+  if (err) console.error(err);
+  console.log('closing db');
+  db && db.close()
+}
