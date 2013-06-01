720055fc36a4c8c5a21ad4400f0fa1c5b1d40938
Thorsten Lorenz <thlorenz@gmx.de>
2013-06-01 12:47:25 -0400

    adding fetch npm json data scripts

---
+++ scripts/fetch-npm-packages.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+'use strict';
+/*jshint asi: true */
+
+var path           =  require('path')
+  , fs             =  require('fs')
+  , packagesStream =  require('../lib/stream-npm-packages')
+  , dataDir        =  process.env.VALUEPACK_DATA || path.join(__dirname, '..', 'data')
+  , jsonPath       =  path.join(dataDir, 'npm-packages.json')
+
+packagesStream().pipe(fs.createWriteStream(jsonPath, { encoding: 'utf8' }))
